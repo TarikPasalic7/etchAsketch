0cfc803219bd46ef9f2d891a9a4c1d58d4198868
unknown <tarik.pasalic777@gmail.com>
2021-05-24 23:41:02 +0200

    reset functionality

--- etchasketch/src/App.js
+++ etchasketch/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, {useEffect,useState} from 'react';
 import './App.css';
+import Box from './components/Box';
 
+
+const elements=[];
 function App() {
+const [gridNumber,setGridNumber]=useState(16)
+
+  const reset =()=>{
+   
+    const val=prompt("How many squares per side do you want");
+    setGridNumber(val);
+
+  }
+
+for (let i = 0; i < gridNumber*gridNumber; i++) {
+  elements.push(i);
+  
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={reset} >Reset Board</button>
+        <button>Black</button>
+        <button>Clear Board</button>
+        <button>Random Color</button>
+      </div>
+     <div className="container">
+     {elements.map((value, index) => {
+        return <Box boxNumber={gridNumber} key={index}/>
+      })}
+    
+    
+       
+     </div>
     </div>
   );
 }
---
+++ etchasketch/src/components/Box.js
@@ -0,0 +1,24 @@
+let boxes = [];
+const Box=({boxNumber})=>{
+ 
+
+
+    return(
+     
+   <div className="bg">
+         
+   
+   {
+    
+  boxNumber}
+  
+
+   </div> 
+  
+
+    )
+
+    
+}
+
+export default Box;
